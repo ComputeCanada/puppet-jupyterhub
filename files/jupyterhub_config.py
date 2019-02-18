f361b75e8f54bcaaaf89692ffc6c8328da0c3317
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2019-02-18 11:21:54 -0500

    Add admin user

--- files/jupyterhub_config.py
+++ files/jupyterhub_config.py
@@ -9,3 +9,6 @@ class MySpawner(SlurmSpawner):
 c.JupyterHub.spawner_class = MySpawner
 c.JupyterHub.hub_ip = '0.0.0.0'
 c.JupyterHub.ip = '127.0.0.1'
+
+c.Authenticator.admin_users = {'admin'}
+c.JupyterHub.admin_access = True
