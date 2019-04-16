66e4af86f3c64e4052b1ad2482f046f12b6f9d20
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2019-04-16 09:27:56 -0400

    Remove squeue from list of cmd that requires sudo

--- files/jupyterhub_config.py
+++ files/jupyterhub_config.py
@@ -1,11 +1,15 @@
 # Configuration file for jupyterhub (postgres example).
 from batchspawner import SlurmSpawner
 class MySpawner(SlurmSpawner):
+    exec_prefix = ""
+    batch_submit_cmd = "sudo -E -u {username} sbatch --parsable"
+    batch_cancel_cmd = "sudo -u {username} scancel {jobid}"
     @property
     def batch_script(self):
         with open('/etc/jupyterhub/submit.sh', 'r') as script_template:
             script = script_template.read()
         return script
+
 c.JupyterHub.spawner_class = MySpawner
 c.JupyterHub.hub_ip = '0.0.0.0'
 c.JupyterHub.ip = '127.0.0.1'
