5d77f0a0fef9841ed2ce8046d64c13cdaa6b607b
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2020-03-27 10:19:42 -0400

    Make sure PIP_PREFIX is the first thing in sys.path

--- files/usercustomize.py
+++ files/usercustomize.py
@@ -5,4 +5,11 @@ import sys
 pip_prefix = os.environ.get('PIP_PREFIX', None)
 if pip_prefix:
     path = os.path.join(pip_prefix, 'lib', 'python' + sys.version[:3], 'site-packages')
+    # Make sure the PIP_PREFIX is prepended to sys.path and not appended
+    # by keeping a reference to the old path list, then replace it by a clean list
+    # to which site will add our pip_prefix path, then we merge the old sys.path
+    # content.
+    prev_syspath = sys.path
+    sys.path = []
     site.addsitedir(path)
+    sys.path.extend(prev_syspath)
