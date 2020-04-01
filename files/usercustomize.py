a3ab150add6d5ae8bf7add395d1fe6e28f4cd4f7
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2020-04-01 12:08:45 -0400

    Fix usercustomize

--- files/usercustomize.py
+++ files/usercustomize.py
@@ -8,6 +8,6 @@ if pip_prefix:
     # Make sure the PIP_PREFIX is prepended to sys.path and not appended
     # by keeping the sys path list length, all paths that were appended at the end
     # are moved at the beginning and the rest of the paths are appened at the end.
-    len_syspath = sys.path
+    len_syspath = len(sys.path)
     site.addsitedir(path)
     sys.path = sys.path[len_syspath:] + sys.path[:len_syspath]
