890102925910658eb85dee6eaf4f940badc7d998
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2023-08-29 12:39:19 +0000

    Fix definition of user environment when using python > 3.9

--- files/usercustomize.py
+++ files/usercustomize.py
@@ -4,7 +4,7 @@ import sys
 
 pip_prefix = os.environ.get('PIP_PREFIX', None)
 if pip_prefix:
-    path = os.path.join(pip_prefix, 'lib', 'python' + sys.version[:3], 'site-packages')
+    path = os.path.join(pip_prefix, 'lib', 'python' + '.'.join(map(str, sys.version_info[:2])), 'site-packages')
     # Make sure the PIP_PREFIX is prepended to sys.path and not appended
     # by keeping the sys path list length, all paths that were appended at the end
     # are moved at the beginning and the rest of the paths are appened at the end.
