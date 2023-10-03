ComputeCanada/puppet-jupyterhub

commit 5896cfd4d68b48a5702e5062ae2b5671b07c9ad6

Simplify jupyterhub::base
The three classes added useless complexity.

data/common.yaml
@@ -1,5 +1,11 @@
 # jupyterhub/data/common.yaml
 ---
+
+nodejs::repo_url_suffix: '18.x'
+# for 18.x, package npm is an alias for package nodejs
+# it should therefore not be absent nor removed
+nodejs::npm_package_name: false
+
 jupyterhub::notebook::version: 6.4.12
 jupyterhub::jupyterhub::version: 2.3.1
 jupyterhub::pamela::version: 1.1.0

manifests/kernel.pp
@@ -1,33 +1,39 @@
-class jupyterhub::kernel::venv(
-  Stdlib::Absolutepath $python = '/usr/bin/python3',
+# 
+class jupyterhub::kernel::venv (
   Stdlib::Absolutepath $prefix = '/opt/ipython-kernel',
   Array[String] $packages = [],
   Hash $pip_environment = {}
 ) {
+  $python3_bin  = lookup('jupyterhub::python3::bin')
+  $python3_path = lookup('jupyterhub::python3::path')
 
   $pip_version = lookup('jupyterhub::pip::version')
 
   exec { 'kernel_venv':
-    command => "${python} -m venv --system-site-packages ${prefix}",
+    command => "${python3_bin} -m venv --system-site-packages ${prefix}",
     creates => "${prefix}/bin/python",
+    path    => [$python3_path],
   }
 
   exec { 'upgrade_pip_setuptools':
-    command     => "${prefix}/bin/pip install --no-cache-dir --upgrade pip==${pip_version} setuptools",
+    command     => "pip install --prefix ${prefix} --no-cache-dir --upgrade pip==${pip_version} setuptools",
     subscribe   => Exec['kernel_venv'],
     refreshonly => true,
+    path        => ["${prefix}/bin"],
   }
 
   exec { 'pip_ipykernel':
-    command => "${prefix}/bin/pip install --no-cache-dir ipykernel",
+    command => 'pip install --no-cache-dir ipykernel',
     creates => "${prefix}/bin/ipython",
-    require => Exec['kernel_venv']
+    require => Exec['kernel_venv'],
+    path    => ["${prefix}/bin"],
   }
 
   exec { 'install_kernel':
-    command => "${prefix}/bin/python -m ipykernel install --name python3 --prefix ${::jupyterhub::node::prefix}",
+    command => "python -m ipykernel install --name python3 --prefix ${::jupyterhub::node::prefix}",
     creates => "${::jupyterhub::node::prefix}/share/jupyter/kernels/python3/kernel.json",
-    require => [Exec['pip_ipykernel']]
+    require => [Exec['pip_ipykernel']],
+    path    => ["${prefix}/bin"],
   }
 
   if (!$packages.empty) {
@@ -38,24 +44,25 @@ class jupyterhub::kernel::venv(
     $pkg_string = join($packages, "\n")
 
     file { "${prefix}/kernel-requirements.txt":
-      ensure  => present,
       content => $pkg_string,
     }
 
     exec { 'install_kernel_requirements_nodeps':
-      command     => "${prefix}/bin/pip install --no-deps --no-cache-dir --prefix ${prefix} --upgrade -r ${prefix}/kernel-requirements.txt",
+      command     => "pip install --no-deps --no-cache-dir --prefix ${prefix} --upgrade -r ${prefix}/kernel-requirements.txt",
       subscribe   => File["${prefix}/hub-requirements.txt"],
       refreshonly => true,
       environment => $pip_env_list,
       timeout     => 0,
+      path        => ["${prefix}/bin"],
     }
 
     exec { 'install_kernel_requirements_deps':
-      command     => "${prefix}/bin/pip install --no-cache-dir --prefix ${prefix} --upgrade -r ${prefix}/kernel-requirements.txt",
+      command     => "pip install --no-cache-dir --prefix ${prefix} --upgrade -r ${prefix}/kernel-requirements.txt",
       subscribe   => Exec['install_kernel_requirements_nodeps'],
       refreshonly => true,
       environment => $pip_env_list,
       timeout     => 0,
+      path        => ["${prefix}/bin"],
     }
   }
 }

data/os/RedHat/7.yaml
@@ -1,5 +1,7 @@
 ---
 jupyterhub::pip::version: 21.3.1
 jupyterhub::pycurl::package_name: python36-pycurl
-jupyterhub::python3::package_name: python3
+jupyterhub::python3::pkg: python3
+jupyterhub::python3::bin: python3
+jupyterhub::python3::path: /usr/bin
 jupyterhub::python3::version: 3.6

data/os/RedHat/8.yaml
@@ -1,5 +1,6 @@
 ---
 jupyterhub::pip::version: 22.2.2
-jupyterhub::python3::package_name: python39
+jupyterhub::python3::pkg: python39
+jupyterhub::python3::bin: python39
+jupyterhub::python3::path: /usr/bin
 jupyterhub::python3::version: 3.9
-jupyterhub::base::install::venv::python: /usr/bin/python3.9

data/os/RedHat/9.yaml
@@ -1,5 +1,6 @@
 ---
 jupyterhub::pip::version: 22.2.2
-jupyterhub::python3::package_name: python39
+jupyterhub::python3::pkg: python39
+jupyterhub::python3::bin: python39
+jupyterhub::python3::path: /usr/bin
 jupyterhub::python3::version: 3.9
-jupyterhub::base::install::venv::python: /usr/bin/python3.9

manifests/base.pp
@@ -1,47 +1,31 @@
-class jupyterhub::base(Stdlib::Absolutepath $prefix) {
-  class { 'jupyterhub::base::install::venv':
-    prefix => $prefix
-  }
-
-  contain jupyterhub::base::install::packages
-  contain jupyterhub::base::install::venv
-  Class['jupyterhub::base::install::packages'] -> Class['jupyterhub::base::install::venv']
-}
-
-class jupyterhub::base::install::packages {
-  class { 'nodejs':
-    repo_url_suffix  => '18.x',
-    # for 18.x, package npm is an alias for package nodejs
-    # it should therefore not be absent nor removed
-    npm_package_name => false,
-  }
-  $python3_pkg = lookup('jupyterhub::python3::package_name')
-  ensure_packages([$python3_pkg],
-    {
-      ensure => 'installed',
-    }
-  )
-}
-
-class jupyterhub::base::install::venv(
+class jupyterhub::base (
   Stdlib::Absolutepath $prefix,
-  Stdlib::Absolutepath $python = '/usr/bin/python3',
 ) {
+  include nodejs
+
+  $pip_version = lookup('jupyterhub::pip::version')
+  $python3_version = lookup('jupyterhub::python3::version')
+  $python3_bin     = lookup('jupyterhub::python3::bin')
+  $python3_pkg     = lookup('jupyterhub::python3::pkg')
+  $python3_path    = lookup('jupyterhub::python3::path')
+
+  ensure_packages([$python3_pkg])
+
   file { [$prefix, "${prefix}/bin"]:
     ensure => directory,
   }
 
   exec { 'jupyterhub_venv':
-    command => "${python} -m venv ${prefix}",
+    command => "${$python3_bin} -m venv ${prefix}",
     creates => "${prefix}/bin/python",
+    require => Package[$python3_pkg],
+    path    => [$python3_path],
   }
 
-  $pip_version = lookup('jupyterhub::pip::version')
-  $python3_version = lookup('jupyterhub::python3::version')
-
   exec { 'pip_upgrade_pip':
-    command => "${prefix}/bin/pip install --upgrade --no-cache-dir pip==${pip_version}",
+    command => "pip install --upgrade --no-cache-dir pip==${pip_version}",
     creates => "${prefix}/lib/python${python3_version}/site-packages/pip-${pip_version}.dist-info/",
     require => Exec['jupyterhub_venv'],
+    path    => ["${prefix}/bin/"],
   }
 }

commit 51b5e0b5ecce74ee92ff2fdcb5f2a2bbb835938d

Reintroduce jupyterhub::kernel::venv::python3
removing it in 5896cfd introduced a regression with puppet-magic_castle

data/common.yaml
@@ -30,6 +30,7 @@ jupyterhub::pammfauthenticator::url: https://github.com/cmd-ntrf/pammfauthentica
 jupyterhub::jupyter_desktop_server::url: https://github.com/cmd-ntrf/jupyter-desktop-server/archive/refs/tags/v0.3.0.zip
 
 jupyterhub::kernel::setup: module
+jupyterhub::kernel::venv::python3: "%{lookup('jupyterhub::python3::path')}/%{lookup('jupyterhub::python3:bin')}"
 
 jupyterhub::reverse_proxy::ssl_certificate_path: ''
 jupyterhub::reverse_proxy::ssl_certificate_key_path: ''

manifests/kernel.pp
@@ -1,18 +1,16 @@
 # 
 class jupyterhub::kernel::venv (
+  Stdlib::Absolutepath $python3,
   Stdlib::Absolutepath $prefix = '/opt/ipython-kernel',
   Array[String] $packages = [],
   Hash $pip_environment = {}
 ) {
-  $python3_bin  = lookup('jupyterhub::python3::bin')
-  $python3_path = lookup('jupyterhub::python3::path')
 
   $pip_version = lookup('jupyterhub::pip::version')
 
   exec { 'kernel_venv':
-    command => "${python3_bin} -m venv --system-site-packages ${prefix}",
+    command => "${python3} -m venv --system-site-packages ${prefix}",
     creates => "${prefix}/bin/python",
-    path    => [$python3_path],
   }
 
   exec { 'upgrade_pip_setuptools':