bdc012dfcf4cb9672eefa2383a16ab24d4db93d6
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2025-08-21 11:16:15 -0400

    Replace setup by install_method in init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -177,11 +177,11 @@ class jupyterhub (
 
   $node_prefix = lookup('jupyterhub::node::prefix', String, undef, $prefix)
   $jupyterhub_config_base = parsejson(file('jupyterhub/jupyterhub_config.json'))
-  $kernel_setup = lookup('jupyterhub::kernel::setup', Enum['venv', 'module'], undef, 'venv')
+  $kernel_setup = lookup('jupyterhub::kernel::install_method', Enum['none', 'venv'], undef, 'venv')
   $kernel_prefix = lookup('jupyterhub::kernel::venv::prefix', Stdlib::Absolutepath, undef, '/opt/ipython-kernel')
   $prologue = $kernel_setup ? {
     'venv'   => "export JUPYTER_PATH=${kernel_prefix}/puppet-jupyter:\${JUPYTER_PATH:-}; export VIRTUAL_ENV_DISABLE_PROMPT=1; source ${kernel_prefix}/bin/activate",
-    'module' => '',
+    'none' => '',
   }
   $jupyterhub_config_params = {
     'JupyterHub' => {
